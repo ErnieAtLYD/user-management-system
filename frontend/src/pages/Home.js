add56cc0481552b8646f7c965b333481b69f0173
Ernie Hsiung <ernie@wyncode.co>
2022-04-19 12:11:09 -0400

    refactor: UserTable component

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import { Link } from 'react-router-dom';
+import UserTable from '../components/UserTable';
 
 const Home = () => {
   const [userList, setUserList] = useState([]);
@@ -20,45 +21,7 @@ const Home = () => {
       <Header />
       <main className='container'>
         <h1>User List</h1>
-        <table role='grid'>
-          <thead>
-            <tr>
-              <th scope='col'>ID</th>
-              <th scope='col'>Name</th>
-              <th scope='col'>Email</th>
-              <th scope='col'>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {userList ? (
-              userList.map((user) => (
-                <tr key={user.id}>
-                  <th scope='row'>{user.id}</th>
-                  <td>
-                    <Link to={`/user/${user.id}`}>
-                      {user.first_name} {user.last_name}
-                    </Link>
-                  </td>
-                  <td>{user.email}</td>
-                  <td>
-                    <Link to={`/user/${user.id}/edit`} role='button'>
-                      Edit
-                    </Link>
-                    <Link
-                      to={`/user/${user.id}/delete`}
-                      className='secondary outline'
-                      role='button'
-                    >
-                      Delete
-                    </Link>
-                  </td>
-                </tr>
-              ))
-            ) : (
-              <p>Loading...</p>
-            )}
-          </tbody>
-        </table>
+        <UserTable userList={userList} />
 
         <Link to='user/add' style={{ width: '100%' }} role='button'>
           Add a user
