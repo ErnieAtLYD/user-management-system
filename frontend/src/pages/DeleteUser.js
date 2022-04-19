495564f3b3a9f48efba7ca5ea908e75257d0f4cd
Ernie Hsiung <ernie@wyncode.co>
2022-04-19 12:11:30 -0400

    feat: add delete functionality

--- frontend/src/pages/DeleteUser.js
+++ frontend/src/pages/DeleteUser.js
@@ -1,15 +1,41 @@
+import { useNavigate, useParams } from 'react-router-dom';
+import axios from 'axios';
+
 const DeleteUser = () => {
+  const navigate = useNavigate();
+  const { userId } = useParams();
+  const API_URL = process.env.REACT_APP_SERVER_URL + '/api/v1/users/' + userId;
+
+  const cancelModal = () => {
+    // like we were pressing the back button
+    navigate(-1);
+  };
+
+  const handleModal = () => {
+    axios
+      .delete(API_URL)
+      .then((resp) => {
+        navigate('/');
+      })
+      .catch((e) => {});
+  };
+
   return (
     <>
       <dialog open>
         <article>
           <h3>Delete User</h3>
-          <p>This will delete. Are you sure?</p>
+          <p>This will delete the user. Are you sure?</p>
           <footer>
-            <a href='#cancel' role='button' class='secondary'>
+            <a
+              href='#cancel'
+              onClick={cancelModal}
+              role='button'
+              class='secondary'
+            >
               Cancel
             </a>
-            <a href='#confirm' role='button'>
+            <a href='#confirm' onClick={handleModal} role='button'>
               Confirm
             </a>
           </footer>
