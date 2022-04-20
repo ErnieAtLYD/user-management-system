b0d7a7678abc1a09e0ca748b7e7c2552347a3444
Ernie Hsiung <ernie@wyncode.co>
2022-04-20 16:53:51 -0400

    resolve merge conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Home loggedInUser={user} />} />
         <Route path='login' element={<Login />} />
         <Route path='user/add' element={<AddUser />} />
         <Route path='user/:userId' element={<User />} />
